028f3f8b0fbf3ce03fab13c79881c9cfaec77bbc
rikiya sasaki <rikiya.sasaki.sys@gmail.com>
2017-06-27 11:12:58 +0900

    first commit

---
+++ src/Quiz.java
@@ -0,0 +1,46 @@
+
+public class Quiz {
+
+
+    public static void main(String[] args){
+    //mainを置く
+    	int situ1 = situmon1();
+
+    	//質問内容
+    	//
+    }
+
+
+	public static  int situmon1(){
+		int answer = 0;
+		//内容表示
+		//回答(男性　女性)
+		//ユーザー答え
+		return answer;
+	}
+
+	public void situmon2(){
+
+
+	}
+
+
+   public void situmon3(){
+
+
+	}
+   public void situmon4(){
+
+
+    }
+    public void situmon5(){
+
+
+    }
+
+    public void situmon6(){
+
+
+    }
+
+}
